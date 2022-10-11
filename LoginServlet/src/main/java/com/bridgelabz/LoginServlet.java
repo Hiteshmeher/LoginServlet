f3ec1fa4fafca352a15090e82567aef712580465
Hiteshmeher <meher.hitesh4@gmail.com>
2022-10-11 21:40:05 +0530

    [Hitesh]: extended the Servlet to accept a valid User name

--- LoginServlet/src/main/java/com/bridgelabz/LoginServlet.java
+++ LoginServlet/src/main/java/com/bridgelabz/LoginServlet.java
@@ -9,12 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login servlet testing",
         urlPatterns = {"/LoginServlet"},
         initParams = {
-                @WebInitParam(name = "username", value = "hitesh"),
+                @WebInitParam(name = "username", value = "Hitesh"),
                 @WebInitParam(name = "password", value = "password")
         }
 )
@@ -29,13 +30,14 @@ public class LoginServlet extends HttpServlet {
         String userId = getServletConfig().getInitParameter("username");
         String pass = getServletConfig().getInitParameter("password");
 
-        if (userId.equals(username) && pass.equals(password)){
+        String nameValidate = "^[A-Z]{1}[a-z]{2,}";
+        if (username.equals(userId) && username.matches(nameValidate) && password.equals(pass)){
             req.setAttribute("username", username);
             req.getRequestDispatcher("loginSuccess.jsp").forward(req,resp);
         }else{
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = resp.getWriter();
-            out.write("<font color=red> User name or password is wrong!.</font>");
+            out.write("<b><font color=red> User name or password is wrong!.</font></b>");
             requestDispatcher.include(req, resp);
             out.close();
         }
